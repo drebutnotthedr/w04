6b23bc2b3b9c5c0b3aa03b72828d7a34c4d810de
drebutnotthedr <91042479+drebutnotthedr@users.noreply.github.com>
2021-10-20 20:15:32 +1100

    Add app.py file

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template
+
+
+app = Flask(__name__, template_folder='templates')
+
+
+@app.route('/')
+def index():
+    return render_template('index.html', title='Weather Observations App')
+
+
+app.run(host='0.0.0.0', port=81)
